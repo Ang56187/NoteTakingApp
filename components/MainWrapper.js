bfb76c93e24d40fe60d05392c209fccc0b329f9a
Ang56187 <ax54@live.com>
2020-02-27 21:17:07 +0800

    Added scroll at main page

--- components/MainWrapper.js
+++ components/MainWrapper.js
@@ -1,15 +1,28 @@
 import React,{Component} from 'react';
-import { StyleSheet,Text,View } from 'react-native';
-import HeaderBar from './header-components/HeaderBar'
-import StatusBarBackground from './StatusBarBackground'
+import { StyleSheet,Text,View,FlatList } from 'react-native';
+import HeaderBar from './header-components/HeaderBar';
+import Constants from 'expo-constants';
+
 
 class MainWrapper extends React.Component{
     constructor(props){
         super(props);
         //create new prop and set state as false
+
+        var a;
+        var arr=[];
+        for (a=0;a<100;a++){
+            arr[a] = {
+                id: a+'-32',
+                title: a
+            }
+            
+        }
+
         this.state = {
             onClickedHamburgerBtn: false,
-            onClickedSearchBtn: false
+            onClickedSearchBtn: false,
+            noteTitles: arr
         }
         //new prop
         //bind creates new func to perform same as  both method handleButtonOnClick
@@ -33,14 +46,23 @@ class MainWrapper extends React.Component{
     render(){
         return(
             <View style={styles.container}>
-            <StatusBarBackground style={{backgroundColor:'midnightblue'}}/>
             <HeaderBar 
                 onClickedHamburgerBtn = {this.state.onClickedHamburgerBtn}
                 onClickedSearchBtn = {this.state.onClickedSearchBtn}
                 handleHamburgerBtnOnClick = {this.handleHamburgerBtnOnClick}
                 handleSearchBtnOnClick = {this.handleSearchBtnOnClick}
             />
-            <Text>Why are you running!</Text>
+            <FlatList 
+                contentContainerStyle = {styles.scroll}
+                data={this.state.noteTitles}
+                numColumns={2}
+                renderItem={({ item }) => (
+                    <View style={styles.viewInScroll}>
+                        <Text style={styles.note}>{item.title}</Text>
+                    </View>
+                )}
+                keyExtractor={item=> item.id}
+            />
           </View>
         );
     }
@@ -49,10 +71,25 @@ class MainWrapper extends React.Component{
 const styles = StyleSheet.create({
     container: {
       flex: 1,
+      marginTop: Constants.statusBarHeight,
       backgroundColor: '#ffffff',
       alignItems: 'center',
-      justifyContent: 'flex-start',
+      alignItems: 'stretch'
     },
+    scroll:{
+        alignItems: 'center',
+        width: '100%'
+    },    
+    viewInScroll:{
+        backgroundColor: '#FF3434',
+        alignItems: 'center',
+        padding: 10,
+        margin: 15,
+        height: 50,
+        width: 150
+    },
+    note:{
+    }
   });  
 
 export default MainWrapper;
